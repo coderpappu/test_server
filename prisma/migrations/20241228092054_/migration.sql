a309c982218b108f1e254184e1ce3cf894836fb5
Pappu Dey <pappudeyprogrammer@gmail.com>
2024-12-28 15:23:13 +0600

    new server file added

---
+++ prisma/migrations/20241228092054_/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` VARCHAR(191) NOT NULL,
+    `first_name` VARCHAR(191) NOT NULL,
+    `last_name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `file` VARCHAR(191) NULL,
+    `type` ENUM('SUPER_ADMIN', 'ADMIN', 'SUBSCRIBER') NOT NULL DEFAULT 'SUPER_ADMIN',
+    `status` ENUM('ACTIVE', 'INACTIVE', 'SUSPENDED') NOT NULL DEFAULT 'ACTIVE',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
