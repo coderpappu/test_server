76b83d3db09fb529fa583bab6ece266da91c4dad
Pappu Dey <pappudeyprogrammer@gmail.com>
2024-12-30 16:25:43 +0600

    version changed

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
+import { PrismaClient } from "@prisma/client";
 import { Server } from "http";
 import app from "./app";
 const PORT = 5000;
 
+const prisma = new PrismaClient();
+
 async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1); // Exit the process with failure
+  }
+
   const server: Server = app.listen(PORT, () => {
     console.log("Server is running on", PORT);
   });
